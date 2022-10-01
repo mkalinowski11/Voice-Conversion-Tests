295916dc5c5bb5654c30178df6ebdbb250b75827
mkalinowski11 <michal.kalinowski321@gmail.com>
2022-10-01 16:54:05 +0200

    Initial commit from cyclegan net basic

---
+++ Cyclegan-basic/config.py
@@ -0,0 +1,19 @@
+import torch
+
+class Config:
+  def __init__(self):
+    self.DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+    self.BATCH_SIZE = 1
+    self.LEARNING_RATE = 1e-5
+    self.LAMBDA_IDENTITY = 10.0
+    self.LAMBDA_CYCLE = 10
+    self.NUM_WORKERS = 1
+    self.NUM_EPOCHS = 1000
+    self.CURRENT_EPOCH = 0
+    self.LOAD_MODEL = False
+    self.CHECKPOINT_GEN_TARGET = None
+    self.CHECKPOINT_GEN_SRC = None
+    self.CHECKPOINT_DISC_TARGET = None
+    self.CHECKPOINT_TRG_SRC = None
+
+config = Config()
