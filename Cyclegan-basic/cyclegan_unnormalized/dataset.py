38bc8a22088803104204a5e336ba92c25e97d56e
Michał Kalinowski <74048743+mkalinowski11@users.noreply.github.com>
2023-02-16 08:39:57 +0100

    modified sources (#10)

--- Cyclegan-basic/cyclegan_unnormalized/dataset.py
+++ Cyclegan-basic/cyclegan_unnormalized/dataset.py
@@ -4,41 +4,9 @@ from torch.utils.data import Dataset
 import librosa
 import random
 import torch
+from project_utils import fit_sound, preprocess_sound, amp_to_decibel
+from project_utils import SAMPLE_RATE
 
-SAMPLE_RATE = 16000
-CUTOFF = 64000
-SPECT_CUTOFF = 501
-EPS=1e-6
-RANDOM_SIGNAL_LEVEL_DB = -40.0
-
-def preprocess_sound(audio):
-  audio_stft = librosa.stft(audio, n_fft=512)
-  amplitude, phase = np.abs(audio_stft), np.angle(audio_stft)
-  return amplitude, phase
-
-def convert_to_complex(amplitude, phase):
-  return amplitude * np.vectorize(complex)(np.cos(phase), np.sin(phase))
-
-def amp_to_decibel(S, ref = 1.0):
-  return 10 * np.log10( (S)  / ref)
-
-def decibel_revert(db):
-  return 10 ** (db / 10)
-
-def signal_pad(signal, fixed_length = CUTOFF, noise_level = RANDOM_SIGNAL_LEVEL_DB):
-  pad_length = fixed_length - signal.shape[0]
-  pad = (np.random.rand(pad_length) - 0.5) * 2 * decibel_revert(noise_level)
-  sound_extended = np.concatenate((signal, pad), axis=0)
-  return sound_extended
-
-def fit_sound(wav, cutoff = CUTOFF):
-  if wav.shape[0] < cutoff:
-    signal_padding = signal_pad(wav, fixed_length = cutoff)
-    return signal_padding
-  return wav[:cutoff]
-
-def complex_to_sound(stft_signal):
-  return librosa.istft(stft_signal)
 
 class Voice_Dataset(Dataset):
     def __init__(self, source_voice_path, target_voice_path):
@@ -61,26 +29,24 @@ class Voice_Dataset(Dataset):
         return self.dataset_length
 
     def __getitem__(self, index):
-      sequence_idx = self.ids[index]
-      src_voice = os.path.join(
+        sequence_idx = self.ids[index]
+        src_voice = os.path.join(
                                 self.source_voice_path,
                                 self.source_voices[sequence_idx]
                                 )
-      target_voice = os.path.join(
+        target_voice = os.path.join(
                                   self.target_voice_path,
                                   self.target_voices[sequence_idx]
                                   )
-      src_voice, sr = librosa.load(src_voice, sr = SAMPLE_RATE)
-      target_voice, sr = librosa.load(target_voice, sr = SAMPLE_RATE)
-      #
-      src_voice, target_voice = fit_sound(src_voice), fit_sound(target_voice)
-      # phase is not required for training
-      src_voice_amp, _ = preprocess_sound(src_voice)
-      target_voice_amp, _ = preprocess_sound(target_voice)
-      #
-      src_voice_amp = amp_to_decibel(src_voice_amp)
-      target_voice_amp = amp_to_decibel(target_voice_amp)
-      # convert to torch
-      src_voice_amp = torch.from_numpy(src_voice_amp)
-      target_voice_amp = torch.from_numpy(target_voice_amp)
-      return src_voice_amp.unsqueeze(0).to(torch.float32), target_voice_amp.unsqueeze(0).to(torch.float32)
+        src_voice, _ = librosa.load(src_voice, sr = SAMPLE_RATE)
+        target_voice, _ = librosa.load(target_voice, sr = SAMPLE_RATE)
+        # set unified size of sound wave
+        src_voice, target_voice = fit_sound(src_voice), fit_sound(target_voice)
+        src_voice_amp, _ = preprocess_sound(src_voice)
+        target_voice_amp, _ = preprocess_sound(target_voice)
+        
+        src_voice_amp = amp_to_decibel(src_voice_amp)
+        target_voice_amp = amp_to_decibel(target_voice_amp)
+        src_voice_amp = torch.from_numpy(src_voice_amp)
+        target_voice_amp = torch.from_numpy(target_voice_amp)
+        return src_voice_amp.unsqueeze(0).to(torch.float32), target_voice_amp.unsqueeze(0).to(torch.float32)
