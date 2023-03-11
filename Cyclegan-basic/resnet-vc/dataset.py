35f21e255241f23b3c415b801036b04bf84c8bc6
Michał Kalinowski <74048743+mkalinowski11@users.noreply.github.com>
2023-03-11 11:48:23 +0100

    Mel data prep resnet (#16)
    * corrected main save model func
    
    * modified dataset and voice processing methods
    
    * corrected sources for new mel feature extraction

--- Cyclegan-basic/resnet-vc/dataset.py
+++ Cyclegan-basic/resnet-vc/dataset.py
@@ -1,50 +1,60 @@
 import os
 from torch.utils.data import Dataset
-import librosa
+import numpy as np
 import random
 import torch
-from project_utils import fit_sound, preprocess_sound, amp_to_decibel
-from project_utils import SAMPLE_RATE
+from project_utils import get_spectrograms, add_padding
 
 class Voice_Dataset(Dataset):
-    def __init__(self, source_voice_path, target_voice_path):
+    def __init__(self, source_voice_path, target_voice_path, target_mel_height = 200):
         self.source_voice_path = source_voice_path
         self.target_voice_path = target_voice_path
+        self.target_mel_height = target_mel_height
 
         self.source_voices = os.listdir(self.source_voice_path)
         self.target_voices = os.listdir(self.target_voice_path)
-        self.source_voices.sort()
-        self.target_voices.sort()
 
         self.source_voices_len = len(self.source_voices)
         self.target_voices_len = len(self.target_voices)
-
         self.dataset_length = min(self.source_voices_len, self.target_voices_len)
-        self.ids = [idx for idx in range(self.dataset_length)]
+        
+        src_mels, trg_mels, mean, std = self.__prepare_dataset()
+        self.src_mels = src_mels
+        self.trg_mels = trg_mels
+        self.dataset_mean = mean
+        self.dataset_std = std
+        self.ids = list(range(self.dataset_length))
         random.shuffle(self.ids)
-
+    
+    def __prepare_dataset(self):
+        src_mels = []
+        trg_mels = []
+        print("start preparing dataset")
+        for voice in self.source_voices:
+            mel = get_spectrograms(os.path.join(self.source_voice_path, voice))
+            mel = add_padding(mel, self.target_mel_height)
+            src_mels.append(mel)
+        for voice in self.target_voices:
+            mel = get_spectrograms(os.path.join(self.target_voice_path, voice))
+            mel = add_padding(mel, self.target_mel_height)
+            trg_mels.append(mel)
+        src_mels, trg_mels = np.array(src_mels), np.array(trg_mels)
+        stacked_mels = np.vstack((src_mels, trg_mels))
+        mean = np.mean(stacked_mels)
+        std = np.std(stacked_mels)
+        # Normalization
+        src_mels = (src_mels - mean) / std
+        trg_mels = (trg_mels - mean) / std
+        print("finished preparing dataset")
+        return src_mels, trg_mels, mean, std
+        
+    def shuffle_ids(self):
+        random.shuffle(self.ids)
+    
     def __len__(self):
         return self.dataset_length
 
     def __getitem__(self, index):
         sequence_idx = self.ids[index]
-        src_voice = os.path.join(
-                                self.source_voice_path,
-                                self.source_voices[sequence_idx]
-                                )
-        target_voice = os.path.join(
-                                  self.target_voice_path,
-                                  self.target_voices[sequence_idx]
-                                  )
-        src_voice, _ = librosa.load(src_voice, sr = SAMPLE_RATE)
-        target_voice, _ = librosa.load(target_voice, sr = SAMPLE_RATE)
-        # set unified size of sound wave
-        src_voice, target_voice = fit_sound(src_voice), fit_sound(target_voice)
-        src_voice_amp, _ = preprocess_sound(src_voice)
-        target_voice_amp, _ = preprocess_sound(target_voice)
-        
-        src_voice_amp = amp_to_decibel(src_voice_amp)
-        target_voice_amp = amp_to_decibel(target_voice_amp)
-        src_voice_amp = torch.from_numpy(src_voice_amp)
-        target_voice_amp = torch.from_numpy(target_voice_amp)
-        return src_voice_amp.unsqueeze(0).to(torch.float32), target_voice_amp.unsqueeze(0).to(torch.float32)
+        return torch.tensor(self.src_mels[sequence_idx]).unsqueeze(0).to(torch.float32),\
+                torch.tensor(self.trg_mels[sequence_idx]).unsqueeze(0).to(torch.float32)
