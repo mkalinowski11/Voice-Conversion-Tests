d6530075ac1e05ac004e5761c8cd8cc5b0a15afd
mkalinowski11 <michal.kalinowski321@gmail.com>
2023-08-20 23:36:50 +0200

    Corrected form display

--- form_app/flask_app/app.py
+++ form_app/flask_app/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, redirect, url_for
 import os
 # from flask_sqlalchemy import SQLAlchemy
 
-VOICE_DATA = 'static'
+VOICE_DATA = os.path.join('static', 'voice_data')
 app = Flask(__name__)
+app.secret_key = 'abracadabra' 
 
 def parse_answers(answers):
     if not any(map(lambda a : len(a) == 0, answers)):
@@ -18,17 +19,23 @@ def index():
         for spk_id in os.listdir(VOICE_DATA):
             data.append(request.form.get(spk_id))
         if parse_answers(data):
-            # save to DB
-            print("OK")
+            print(f'Succesfully added data: {data}')
         else:
             print("Not ok")
     return render_template(
-        "form.html",
-        title="Weryfikacja jakości głosu",
-        speaker_data = os.listdir(VOICE_DATA),
-        samples = os.listdir(os.path.join(
-            VOICE_DATA, os.listdir(VOICE_DATA)[0]
-        )))
+            "form.html",
+            title="Weryfikacja jakości głosu",
+            speaker_data = os.listdir(VOICE_DATA),
+            samples = os.listdir(os.path.join(
+                VOICE_DATA, os.listdir(VOICE_DATA)[0]
+            )),
+            labels = [
+                'Źródło A',
+                'Głos docelowy B',
+                'Konwersja A->B',
+                'Konwersja B->A'
+            ]
+        )
 
 if __name__ == "__main__":
     app.run(debug=True)
